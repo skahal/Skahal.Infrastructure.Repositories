53b83629260b9ff9d7f2454e03bd0f7d76377547
Diego Giacomelli <giacomelli@gmail.com>
2013-11-16 00:33:45 -0200

    Travis tests.

--- src/FunctionalTests/MongoDB/MongoDBRepositoryBaseTest.cs
+++ src/FunctionalTests/MongoDB/MongoDBRepositoryBaseTest.cs
@@ -34,7 +34,13 @@ namespace Skahal.Infrastructure.Repositories.FunctionalTests
 
 			if (!File.Exists(mongodPath)) {
 				mongodPath = "/etc/rc.d/init.d/mongod";
+
+				if (!File.Exists(mongodPath)) {
+					mongodPath = "mongod";
+				}
 			}
+
+
 
 			ProcessHelper.Run (mongodPath, "--dbpath {0} --logpath {1}".With (dbPath, logPath), false);
 		
